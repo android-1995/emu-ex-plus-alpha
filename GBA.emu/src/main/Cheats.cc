b81d6dafcf5941ba0e2d2d139d05c169a3dda357
zhangquan <1066298406@qq.com>
2021-04-23 10:00:37 +0800

    金手指完成？

--- GBA.emu/src/main/Cheats.cc
+++ GBA.emu/src/main/Cheats.cc
@@ -248,7 +248,8 @@ void setCheatListAiWu(std::list<std::string> cheats)
     //然后写入
     for (std::list<std::string>::iterator it = cheats.begin(); it != cheats.end(); it++)
     {
-        cheatsAddGSACode(gGba.cpu, it.c_str(), "", false);
+        std::string& cheat = *it;
+        cheatsAddGSACode(gGba.cpu, cheat.c_str(), "", false);
     }
 }
 //endregion
