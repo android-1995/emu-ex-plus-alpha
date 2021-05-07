fa733713d9a50e2f9edb49c08150600c1ec30793
zhangquan <1066298406@qq.com>
2021-05-07 09:30:20 +0800

    改自动存档的名字

--- GBC.emu/src/main/Main.cc
+++ GBC.emu/src/main/Main.cc
@@ -124,9 +124,28 @@ void EmuSystem::reset(ResetMode mode)
 	gbEmu.reset();
 }
 
+const char *saveSlotCharAiWu(int slot)
+{
+    switch(slot)
+    {
+        case -1: return "10";
+        case 0: return "0";
+        case 1: return "1";
+        case 2: return "2";
+        case 3: return "3";
+        case 4: return "4";
+        case 5: return "5";
+        case 6: return "6";
+        case 7: return "7";
+        case 8: return "8";
+        case 9: return "9";
+        default: return "10";
+    }
+}
+
 FS::PathString EmuSystem::sprintStateFilename(int slot, const char *statePath, const char *gameName)
 {
-	return FS::makePathStringPrintf("%s/%s.0%c.gqs", statePath, gameName, saveSlotCharUpper(slot));
+	return FS::makePathStringPrintf("%s/%s.%s.gqs", statePath, gameName, saveSlotCharAiWu(slot));
 }
 
 EmuSystem::Error EmuSystem::saveState(const char *path)
